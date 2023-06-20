588f262c8860e649032bced66032a37dc9ae1292
Moonsungjeong <gmsj159@gmail.com>
2023-06-21 00:23:21 +1000

    First commit

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const addInterface = (x) => {
+    return x.a + x.b;
+};
+const add = (a, b) => {
+    if (b) {
+        return a + b;
+    }
+    else {
+        return a;
+    }
+};
+app.get("/", (req, res) => {
+    add(1);
+    addInterface({ a: 1, b: 2 });
+    res.status(200).json("start NodeTS!!!~~~~");
+});
+app.listen(3001, () => {
+    console.log("started");
+});
