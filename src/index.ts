fccd77350eba54c2e1324bed8f2c7b72ec0baa80
Moonsungjeong <gmsj159@gmail.com>
2023-07-02 17:54:18 +1000

    update configuration

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import express from "express";
 
 const app = express();
 
-interface Params {
+// type is for others (personal preference). 구조체??
+type Params2 = {
   a: number;
   b: number;
-}
-type Fn = () => void;
-type Add = (x: Params) => number;
-
-const addInterface: Add = (x) => {
-  return x.a + x.b;
 };
+// 그냥 함수
+type Fn = () => void;
 
+// 변수에 type 선언
 const add = (a: number, b?: number): number => {
   if (b) {
     return a + b;
@@ -21,9 +19,21 @@ const add = (a: number, b?: number): number => {
   }
 };
 
+// interface is for object (personal preference). Class??
+interface Params {
+  a: number;
+  b: number;
+}
+// type을 이용한 함수 정의 + interface 이용
+type Add = (x: Params) => number;
+
+const addInterface: Add = (x) => {
+  return x.a + x.b;
+};
+
 app.get("/", (req: any, res: any) => {
-  add(1);
-  addInterface({ a: 1, b: 2 });
+  console.log(add(1));
+  console.log(addInterface({ a: 1, b: 2 }));
   res.status(200).json("start NodeTS!!!~~~~");
 });
 
